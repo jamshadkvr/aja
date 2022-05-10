7b3ec361ba8a67c3a6e014610186a4867c46b3d7
AA-AVR <81420882+AA-AVR@users.noreply.github.com>
2022-05-11 00:21:10 +0530

    Create ping.py

---
+++ Ajax/bot/plugins/ping.py
@@ -0,0 +1,13 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters, Client
+import time
+import shutil, psutil
+from utils_bot import *
+from Ajax import StartTime
+
+@Client.on_message(filters.command('ajax') & filters.private & ~filters.edited)
+async def stats(bot, update):
+  currentTime = readable_time((time.time() - StartTime))
+  botstats = f'<b>Bot Uptime:</b> {currentTime}' 
+            
+  await update.reply_text(botstats)
