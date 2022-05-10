6a6b129ed161c98432b4ae0e421017863e21dc84
AA-AVR <81420882+AA-AVR@users.noreply.github.com>
2022-05-11 00:16:58 +0530

    Create utils_bot.py

---
+++ utils_bot.py
@@ -0,0 +1,69 @@
+import logging
+import os
+import threading
+import time
+from asyncio import TimeoutError
+from pyrogram import filters
+
+class setInterval:
+    def __init__(self, interval, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = threading.Event()
+        thread = threading.Thread(target=self.__setInterval)
+        thread.start()
+
+    def __setInterval(self):
+        nextTime = time.time() + self.interval
+        while not self.stopEvent.wait(nextTime - time.time()):
+            nextTime += self.interval
+            self.action()
+
+    def cancel(self):
+        self.stopEvent.set()
+
+def get_readable_time(seconds: int) -> str:
+    result = ''
+    (weeks, remainder) = divmod(seconds, 604800)
+    days = int(weeks)
+    if weeks != 0:
+        result += f'{weeks}w'
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
+
+
+
+def readable_time(seconds: int) -> str:
+    result = ''
+    (weeks, remainder) = divmod(seconds, 604800)
+    days = int(weeks)
+    if weeks != 0:
+        result += f'{weeks}w'
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
