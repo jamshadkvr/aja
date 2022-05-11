58ca12489f4f3b9534cd7f0da6e9fd6c311adae3
AA-AVR <81420882+AA-AVR@users.noreply.github.com>
2022-05-11 10:50:22 +0530

    Update extra.py

--- Ajax/bot/plugins/extra.py
+++ Ajax/bot/plugins/extra.py
@@ -5,8 +5,8 @@ import shutil, psutil
 from utils_bot import *
 from Ajax import StartTime
 
-@Client.on_message(filters.command('status') & filters.private & ~filters.edited)
-async def status(bot, update):
+@Client.on_message(filters.command('stats') & filters.private & ~filters.edited)
+async def stats(bot, update):
   currentTime = readable_time((time.time() - StartTime))
   botstats = f'<b>Bot Uptime:</b> {currentTime}'             
   await update.reply_text(botstats)
