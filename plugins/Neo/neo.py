242ec47ee853636e821882fb18a4859220d267a0
AA-AVR <81420882+AA-AVR@users.noreply.github.com>
2022-05-11 14:41:07 +0530

    Update neo.py

--- plugins/Neo/neo.py
+++ plugins/Neo/neo.py
@@ -21,14 +21,10 @@ async def ping(_, message):
     time_taken_s = (end_t - start_t) * 1000
     uptime = time.strftime("%HH | %MM | %SS", time.gmtime(time.time() - BOT_START_TIME))   
     await avr.edit(f"‹ ᴄᴜʀʀᴇɴᴛ ʙᴏᴛ sᴛᴀᴛᴜs ›\n\n‹› ᴘᴏɴɢ : {time_taken_s:.3f} ms\n‹› ʙᴏᴛ ᴜᴘᴛɪᴍᴇ : {uptime}")
-    start_time = datetime.now()
-    end_time = datetime.now()
-    print('Duration: {}'.format(end_time - start_time))
-
-
+    
 @Client.on_message(filters.command("ping"))
 async def ping(_, message):
     start_time = datetime.now()
     avr = await message.reply_text("‹○○○›")
     end_time = datetime.now()
-    await avr.edit(f"'Duration: {}'.format(end_time - start_time"))
+    await avr.edit(f"'Duration: {}'.format(end_time - start_time)")
