639e4d35d4e1dc33f6e24561a51f0144ea02bb35
AA-AVR <81420882+AA-AVR@users.noreply.github.com>
2022-05-11 11:05:50 +0530

    Create neo.py

---
+++ plugins/Neo/neo.py
@@ -0,0 +1,39 @@
+#@Aadhi000 #Neo #neo
+
+import asyncio
+import os
+import math
+import time
+import heroku3
+import requests
+from pyrogram import Client, filters
+
+#=====================================================
+BOT_START_TIME = time.time()
+#=====================================================
+
+@Client.on_message(filters.command("ping"))
+async def ping(_, message):
+    start_t = time.time()  
+    avr = await message.reply_text(
+             text="•"
+         )
+         await asyncio.sleep(0.5)
+         reply2 = await reply1.edit_text(
+             text="••"
+         ) 
+         await asyncio.sleep(0.5)
+         reply3 = await reply2.edit_text(
+             text="•••"
+         )
+         await asyncio.sleep(0.5)
+         reply4 = await reply3.edit_text(
+             text="••••"
+         )
+         await reply4.delete()  
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    uptime = time.strftime("%HH | %MM | %SS", time.gmtime(time.time() - BOT_START_TIME))   
+    await avr.edit(f"‹ ᴄᴜʀʀᴇɴᴛ ʙᴏᴛ sᴛᴀᴛᴜs ›\n\n‹› ᴘᴏɴɢ : {time_taken_s:.3f} ms\n‹› ʙᴏᴛ ᴜᴘᴛɪᴍᴇ : {uptime}")
+    await asyncio.sleep(10)
+    await avr.delete()
